af99ad1e4a9b81b7ed89f698676c0b5b06c5fddd
habil99 <abiyevhabil3@gmail.com>
2023-01-25 12:45:35 +0400

    Convert scss files to css

--- src/pages/home/HomeBanner.js
+++ src/pages/home/HomeBanner.js
@@ -130,14 +130,12 @@ const HomeBanner = ({ showLoginModal, showSignUpModal, createWish }) => {
             </button>
           </div>
         </nav>
-        <div className="flex min-h-[80vh] flex-col items-center justify-content-center mt-[8vh]">
-          <div className="flex flex-col -mt-16 md:mt-0 items-center justify-center align-self-center justify-self-center">
-            <h1 className="banner__title">
-              Give <br />
-              yourself <br />
-              emotions
+        <div className="flex flex-1 flex-col items-center justify-content-center mt-[8vh]">
+          <div className="flex flex-col md:mb-[30px] -mt-16 md:mt-0 items-center justify-center align-self-center justify-self-center">
+            <h1 className="banner__title max-w-[520px]">
+              Everything starts with a Wish
             </h1>
-            <div className="flex md:mb-[130px] flex-wrap items-center gap-8 mt-4 md:mt-8 md:px-0 px-12">
+            <div className="flex flex-wrap items-center gap-8 mt-4 md:mt-8 md:px-0 px-12">
               <button
                 className="tracking-[0.01em] flex-1 flex-shrink-0 min-w-max border border-solid border-white rounded-[40px] bg-white py-3 md:py-6 px-14 text-[16px] leading-[1.4] text-[#2D008D] font-semibold"
                 onClick={createWish}
