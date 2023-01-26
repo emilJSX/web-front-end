331d0a9c71e8e0abea9211e8bbed04093e53650f
habil99 <abiyevhabil3@gmail.com>
2023-01-26 11:59:45 +0400

    Fix country and date bugs in profile edit

--- src/provider/AppProvider.js
+++ src/provider/AppProvider.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux';
 import { Layout } from '../shared/components/layout';
-import { store } from '../store'
+import { store } from '../store/index.js'
 
 const AppProvider = ({ children }) => {
     return (
